222189d21511f90dbf6dc66401908d879084d01f
ErrQD0303 <quocdat3396@gmail.com>
2026-04-21 18:26:49 +0700

    chore(sync): pull v1.1.0 release fixes back into development

--- terraform/scripts/build-frontend-app.sh
+++ terraform/scripts/build-frontend-app.sh
@@ -1,7 +1,5 @@
-#! /usr/bin/bash
+#!/usr/bin/bash
 
-# This script is responsible for building the frontend application using npm. It should be run from the root of the project directory.
-# From this directory, you can run the following command to build the frontend application:
 cd ../ClientApp
 export PATH="/c/Users/ADMIN/AppData/Roaming/nvm/v22.12.0:/c/Program Files/nodejs:$PATH"
 npm install
